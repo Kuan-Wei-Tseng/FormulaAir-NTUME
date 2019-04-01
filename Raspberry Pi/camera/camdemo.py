ac298f9b69e1235af799e4f133eda2645d7314a5
Tseng Kuan Wei <b05505008@ntu.edu.tw>
2019-04-01 17:08:31 +0800

    Add real-time demo

--- Raspberry Pi/camera/camdemo.py
+++ Raspberry Pi/camera/camdemo.py
@@ -54,7 +54,7 @@ while (True):
 	print (time.perf_counter()-t0)	
 	cv2.imshow('My Image', img)
   	if cv2.waitKey(300) & 0xFF == ord('q'):
-    	break
+  		break
 
 cv2.destroyAllWindows()
 #cv2.imwrite('marked.bmp',img)
