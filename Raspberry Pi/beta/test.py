5883cb3e3707443b11ed29c67cd4c64463288019
KuanWei-Tseng <b05505008@ntu.edu.tw>
2019-04-03 10:52:39 +0800

    test

--- Raspberry Pi/beta/test.py
+++ Raspberry Pi/beta/test.py
@@ -8,13 +8,18 @@ def showimage():
     cv2.imshow('Hello',x)
     cv2.waitKey(5000)
 
-#thread1 = threading.Thread(target=showimage, name='T1')
-#thread1.start()
+thread1 = threading.Thread(target=showimage, name='T1')
+thread1.start()
+print('hello world')
+time.sleep(10)
+print('hi')
+thread1.join()
 
-
+'''
 for i in range(0,10):
 	x = cv2.imread('marked.bmp')
 	print('Hello world')
 	time.sleep(3)
 	cv2.imshow('Hello',x)
 	cv2.waitKey(1)
+'''
