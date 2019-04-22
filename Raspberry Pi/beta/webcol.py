566cc4318f577e0028734d4a0f4547d62b6b70f6
Tseng Kuan Wei <b05505008@ntu.edu.tw>
2019-04-22 16:15:23 +0800

    Add webcol

--- Raspberry Pi/beta/webcol.py
+++ Raspberry Pi/beta/webcol.py
@@ -5,13 +5,18 @@ import cv2
 cap = cv2.VideoCapture(0)
 name = '0422image'
 
-for i in range(0,20):
+i = 1
+
+while True:
 	ret, frame = cap.read()
 	cv2.imshow('fraxme', frame)
-	if cv2.waitKey(0) & 0xFF == ord('q'):
+	if cv2.waitKey(1) & 0xFF == ord('q'):
 		fname = name + str(i) + '.bmp'
 		cv2.imwrite(fname,frame)
 		continue
+	i = i+1
+	if i > 20:
+		break
 
 cap.release()
 cv2.destroyAllWindows()
