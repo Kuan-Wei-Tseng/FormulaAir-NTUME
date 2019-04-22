f877f2943c79adac670f9b9e2cca83555029c75f
KuanWei-Tseng <b05505008@ntu.edu.tw>
2019-04-22 21:56:15 +0800

    config

--- Raspberry Pi/dev/car.py
+++ Raspberry Pi/dev/car.py
@@ -14,12 +14,12 @@ class car:
 	angle = 0
 	def __init__(self):
 		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(SERVO_PIN, GPIO.OUT)
-		self.pwm = GPIO.PWM(SERVO_PIN, PWM_FREQ)
+		GPIO.setup(config.SERVO_PIN, GPIO.OUT)
+		self.pwm = GPIO.PWM(config.SERVO_PIN, config.PWM_FREQ)
 		self.pwm.start(0)
 
 	def ang2pwm(self,angle):
-		DC = (0.05 * PWM_FREQ) + (0.19 * PWM_FREQ * angle / 180)
+		DC = (0.05 * config.PWM_FREQ) + (0.19 * config.PWM_FREQ * angle / 180)
 		return DC
 
 	def setdir(self, val = 0):
