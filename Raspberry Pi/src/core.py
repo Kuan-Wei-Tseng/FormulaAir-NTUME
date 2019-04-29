591d4d0b61fe6a22e4f43ebd9ce22c8ab6c80882
Tseng Kuan Wei <b05505008@ntu.edu.tw>
2019-04-29 20:22:15 +0800

    comment

--- Raspberry Pi/src/core.py
+++ Raspberry Pi/src/core.py
@@ -56,13 +56,14 @@ class core:
 			print(dev)
 			self.mycar.setlev(dev)
 			self.mimg = self.myFinder.markline(self.pts)
+			'''
 			cv2.imshow('demo',self.mimg)
 			if cv2.waitKey(0) == 27:
 				break
 			#x = input('Save the image?')
 			counter = counter + 1
 			#fname = '/home/pi/Desktop/debugimg' + str(counter) + '.bmp'
-			'''
+			
 			if x == '1':
 				cv2.imwrite(fname,img)
 			'''
