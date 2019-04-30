dc10bb1e584e012a60ce6a837c8914d899e26523
Tseng Kuan Wei <b05505008@ntu.edu.tw>
2019-04-30 18:50:37 +0800

    cutheight

--- Raspberry Pi/config/config.py
+++ Raspberry Pi/config/config.py
@@ -9,7 +9,7 @@ C2 = 5
 NUMofCUT = C1+C2
 # Gives an array of the slices' x coordinate.
 CUTHEIGHT = np.linspace(RES[1]-10,RES[1]/2,num = C1).astype(int)
-np.append(CUTHEIGHT,np.linspace(RES[1]/2,20,num = C2).astype(int))
+CUTHEIGHT = np.append(CUTHEIGHT,np.linspace(RES[1]/2,20,num = C2).astype(int))
 print(CUTHEIGHT)
 # Percentage of tolerance when detecting unexpected points:
 tol = 0.3 * RES[0]
