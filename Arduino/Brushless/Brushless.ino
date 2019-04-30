92dc86fab348d748e0b55e55fe57ecf2e470283f
Tseng Kuan Wei <b05505008@ntu.edu.tw>
2019-04-30 18:00:23 +0800

    Arduino

--- Arduino/Brushless/Brushless.ino
+++ Arduino/Brushless/Brushless.ino
@@ -9,29 +9,29 @@ void setup(){
 	Brushless1.attach(9);
 	Serial.begin(9600);
 	Brushless1.write(0);
-	Serial.print("Ready for setting up Brushless Motor.");
+	Serial.println("Ready for setting up Brushless Motor.");
 }
 
 void motorinit(){
-	Serial.print("Start initailizing Brushless Motor.\n");
-	Serial.print("Setting high speed! Wait 2 second! ");
+	Serial.println("Start initailizing Brushless Motor.\n");
+	Serial.println("Setting high speed! Wait 2 second! ");
 	Brushless1.write(180);
 	delay(2000);
-	Serial.print("Setting low speed! Wait 4 sec! ");
+	Serial.println("Setting low speed! Wait 4 sec! ");
 	Brushless1.write(5);
 	delay(4000);
 	Serial.print("MOTOR IS READY! ");
 }
 
 void setspeed(int val){
-	Serial.print('Arduino Setspeed!');
+	Serial.println('Arduino Setspeed!');
 	Brushless1.write(val);
 }
 
 void loop(){
 	String ans;
 	char com[20];
-	Serial.print("Please Provide Some Instructions.");
+	Serial.println("Please Provide Some Instructions.");
 	while(Serial.available()==0){}
 	ans = Serial.readString();
 	ans.toCharArray(com, 20);
@@ -42,7 +42,7 @@ void loop(){
 		case 's':
 			int val;
 			val = atoi(&com[1]);
-			
+
 			setspeed(val);
 			break;
 		default:
