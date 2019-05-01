9e5979a19d0ffb0efe1d7bce7ec4fa385217329b
Tseng Kuan Wei <b05505008@ntu.edu.tw>
2019-05-01 18:40:20 +0800

    Enable outlier rejection

--- Raspberry Pi/beta/stat.py
+++ Raspberry Pi/beta/stat.py
@@ -3,6 +3,11 @@ import numpy as np
 
 x = np.array([250,255,240,243,228,264,490,225,178,252,211]);
 s = np.std(x)
-print(s)
+m = np.mean(x)
+y = (x-m)/s
+r = np.where(abs(y)>1)
+z = np.delete(x,r)
+
+print(z)
 
 
