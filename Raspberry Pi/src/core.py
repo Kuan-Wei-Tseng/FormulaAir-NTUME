243a76b6db738e49f4e2dffc47f156074b15fbaf
Tseng Kuan Wei <b05505008@ntu.edu.tw>
2019-05-02 11:52:40 +0800

    timer

--- Raspberry Pi/src/core.py
+++ Raspberry Pi/src/core.py
@@ -43,7 +43,7 @@ class core:
 			self.myFinder = findline(img)
 			self.pts,self.mimg, self.maploc = self.myFinder.detectline()
 			print(self.pts)
-			if tt < 1000:
+			if tt < 5000:
 				self.maploc = 0
 			dev = self.mycontrol.detlevel(self.pts,self.maploc)
 			print(dev)
